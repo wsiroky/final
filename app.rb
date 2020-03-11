b6bff4b3d1e564b0fd88d646418b78ebe21e092f
wsiroky <wsiroky@gmail.com>
2020-03-11 20:43:10 +0000

    Updated without heroku

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "bcrypt"   
+require "geocoder"                                                                  #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -16,3 +17,75 @@ after { puts; }                                                                 
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts events_table.all
+    @events = events_table.all.to_a
+    view "events"
+end
+
+get "/events/:id" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    @rsvps = rsvps_table.where(event_id: @event[:id])
+    @going_count = rsvps_table.where(event_id: @event[:id], going: true).count
+    @users_table = users_table
+    @event_address = @event[:address]
+    results = Geocoder.search(@event_address)
+    @coordinates = results.first.coordinates # => [lat, long]
+    @lat = @coordinates[0]
+    @long = @coordinates[1]
+    @lat_long = "#{@lat},#{@long}"
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+get "/events/:id/rsvps/create" do
+    puts params
+    @event = events_table.where(id: params["id"]).to_a[0]
+    rsvps_table.insert(event_id: params["id"],
+                       user_id: session["user_id"],
+                       going: params["going"])
+    view "create_rsvp"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -8,28 +8,42 @@ DB = Sequel.connect(connection_string)                                          
 DB.create_table! :events do
   primary_key :id
   String :title
-  String :description, text: true
+  String :specials, text: true
   String :date
   String :location
+  String :address
 end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "St. Patty’s Day Bar Review", 
+                    specials: "All-you-can-drink wristbands for $20, raffle tickets for a $50 and $100 Duffy’s Gift Card ($5 for 3 or $10 for 10). Note that these must be purchased at the SFPIF table in the atrium between 3/2 and 3/12",
+                    date: "Thursday, March 12th from 9pm – 1 am",
+                    location: "Duffy’s Tavern",
+                    address: "420 W Diversey Pkwy, Chicago, IL, 60614")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "JD / MBA Bar Review @ the Concrete Cowboy", 
+                    specials: "1/2 off all apps, $3 draft beers, buy one, get one whiskey shots",
+                    date: "Thursday, March 26th from 8:30pm – 2 am",
+                    location: "The Concrete Cowboy",
+                    address: "646 North Franklin Street, Chicago, IL, 60654")
+
+events_table.insert(title: "BLSA Bar Review", 
+                    specials: "Half priced bottles of wine, $5 well cocktails, $15 fish bowls",
+                    date: "Thursday, April 2nd from 10pm – 1:30 am",
+                    location: "Sheffield's Chicago",
+                    address: "3258 North Sheffield Avenue, Chicago, IL 60657")                    
